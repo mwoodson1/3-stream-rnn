36e4e802073b27f474340b1bf9fd6050abfd0a0c
Markus Woodson <markus.woodson1@gmail.com>
2015-12-05 17:19:48 -0500

    fixed loading of training and testing data to be in batch iterator format

--- load_ucf.py
+++ load_ucf.py
@@ -7,7 +7,19 @@ import pickle
 
 NUM_FRAMES = 70  #Number of frames to be selected for each video
 
-def selectRandomFrames(fileName):
+def rand_indices(length):
+	#Generate unique numbers within a range (for random indices of frames)
+	randomIndexSet = []
+	if NUM_FRAMES > length:
+		randomIndexSet = random.sample(range(0, length), length)
+	else:
+		randomIndexSet = random.sample(range(0, length), NUM_FRAMES)
+
+	indices = sorted(randomIndexSet)
+
+	return indices
+
+def selectRandomFrames(fileName,testData):
 	"""
 	Goes through all the preprocessed .mat files for each video and selects a
 	random set of frames for training set
@@ -21,19 +33,11 @@ def selectRandomFrames(fileName):
     #Checking the number of frames in the video
 	length = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
 
-    #Generate unique numbers within a range (for random indices of frames)
-	randomIndexSet = []
-	if NUM_FRAMES > length:
-		randomIndexSet = random.sample(range(0, length), length)
-	else:
-		randomIndexSet = random.sample(range(0, length), NUM_FRAMES)
+	indices = rand_indices(length)
+
 	cap.release()
 
-	indices = sorted(randomIndexSet)
-
 	#Loop through all of the frames and get training frames
-	output_frames = []
-
 	tmp = fileName.split("/")
 	new_fileName = "data/pre-process/cropped/"+tmp[2]+"/"+tmp[3].split(".")[0] +"_cropped.avi"
 
@@ -45,24 +49,25 @@ def selectRandomFrames(fileName):
 
 	pos_frame = cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
 	out = 0
+	wait = 0
 	while(1):
 		ret, frame2 = cap.read()
 		#If no more frames can be read then break out of our loop
 		if(not(ret)):
-			if(out >= len(indices)-1):
+			if(out >= len(indices)-1 or pos_frame >= length-5 or wait > 20):
 				break
 			else:
-				output_frames.append(frame2)
 				cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, pos_frame+1)
 				cv2.waitKey(1000)
+				wait += 1
 		else:
 			pos_frame = cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
-			#print pos_frame
-			if(len(output_frames) >= len(indices)):
-				break
 			#print "im here"
 			if(pos_frame in indices):
-				file_dir = "trainData/cropped/"+tmp[2]+"/"
+				if(testData):
+					file_dir = "testData/cropped/"+tmp[2]+"/"
+				else:
+					file_dir = "trainData/cropped/"+tmp[2]+"/"
 				img_Name = tmp[3].split(".")[0] + "_" + str(int(pos_frame))
 				cv2.imwrite(file_dir+img_Name+".jpg",frame2)
 				out += 1
@@ -121,7 +126,7 @@ def createTrainingSet():
 			#Check if filename exists in training set, otherwise skip
 			if dirs[i]+"/"+filename in keys:
 				print dirs[i]+"/"+filename
-				selectRandomFrames(dirs[i]+"/"+filename)
+				selectRandomFrames(dirs[i]+"/"+filename,False)
 
 def getTestList():
 	"""
@@ -141,7 +146,7 @@ def getTestList():
 	for f in files:
 	    fOpen = open(directory+f, 'r')
 	    for line in fOpen:
-	        fName = "data/UCF-101/"+line
+	        fName = "data/UCF-101/"+line.rstrip()
 	        testVids[fName] = class_ind[line.split("/")[0]]
 	return testVids
 
@@ -160,21 +165,19 @@ def createTestingSet():
 		if not os.path.exists(directory):
 			os.makedirs(directory)
 
-	#Get all the directories in the UCF-101 dataset
-	dirs = [x[0] for x in os.walk("data/UCF-101/")]
 	#Get all the videos in the training list
 	trainVids = getTestList()
 	keys = trainVids.keys()
 
 	#Dictionary to hold filename and the list of random frame indices 
-	trainingFrames = dict.fromkeys(keys)
+	#trainingFrames = dict.fromkeys(keys)
 
-	frames_so_far = np.zeros([len(keys)*NUM_FRAMES,120,160,3])
+	#frames_so_far = np.zeros([len(keys)*NUM_FRAMES,120,160,3])
 
-	import random
-	rand_ind = random.sample(range(len(keys)*NUM_FRAMES),len(keys)*NUM_FRAMES)
+	#import random
+	#rand_ind = random.sample(range(len(keys)*NUM_FRAMES),len(keys)*NUM_FRAMES)
 	
-	outputs = np.zeros([len(keys),1])
+	#outputs = np.zeros([len(keys),1])
 
 	l = len(dirs)
 	cnt = 0
@@ -186,24 +189,19 @@ def createTestingSet():
 		#Loop through every file in the directory
 		for filename in os.listdir(dirs[i]):
 			#Check if filename exists in training set, otherwise skip
+			#print dirs[i]+"/"+filename
 			if dirs[i]+"/"+filename in keys:
-				k = rand_ind[cnt]
-				frames_so_far[k,:,:,:] = selectRandomFrames(dirs[i]+"/"+filename)
-				outputs[k] = trainVids[dirs[i]+"/"+filename] 
-				cnt += 1
-	#np.save("testing_frames",frames_so_far)
-	#np.save("testing_frames_classes", outputs)
-
+				#print "got here"
+				selectRandomFrames(dirs[i]+"/"+filename,True)
 
 def main():
-	createTrainingSet()
-	if(os.path.isfile("training_frames_classes.npy")):
-		print "Found training frame file"
+	if(os.path.isdir("trainData")):
+		print "Found training frame directory"
 	else:
 		createTrainingSet()
 
-	if(os.path.isfile("testing_frames_classes.npy")):
-		print "Found testing frame file"
+	if(os.path.isdir("testData")):
+		print "Found testing frame directory"
 	else:
 		createTestingSet()
 
